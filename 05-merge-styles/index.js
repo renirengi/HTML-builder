fafd368b738881499162fea704b4d7b8168247a7
Olga Bogdanova <TomieForever@gmail.com>
2021-11-04 21:44:14 +0300

    feat: solutions 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,16 +2,19 @@ const fs = require("fs");
 const path = require("path");
 
 const way = path.join(__dirname, 'styles');
-///const stream = fs.createReadStream(way);
 
 
 fs.readdir(way, { withFileTypes: true }, (err, files) => {
     if(err) throw err;
     files.forEach(function(file){
         if (path.extname(file.name)=='.css'){
-            file.on('data', chunk => {
-                console.log(chunk.toString());
-              });    
+            let newWay=path.join(way, file.name);
+            let stream = fs.createReadStream(newWay);
+            let content=[];
+            stream.on('data', chunk => {
+             content.push(chunk.toString())
+            });
+            console.log(content);    
         }
     });
     
